1ab414a8020a977d1efd0e7cba7a21cd4882e155
HalilUcel <hucelsoft@gmail.com>
2022-03-18 06:16:01 +0300

    change image

--- lib/widget_learn.dart
+++ lib/widget_learn.dart
@@ -1,31 +1,65 @@
+import 'package:best_baltanem/best_baltanem.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Learn Widget"),
-        centerTitle: true,
-      ),
+      appBar: appBar(context),
+      backgroundColor: Colors.black,
       body: WidgetLearn,
+    );
+  }
+
+  AppBar appBar(BuildContext context) {
+    return AppBar(
+      title: Text("GridView-FittedBox Widget"),
+      centerTitle: true,
     );
   }
 
   Widget get WidgetLearn {
     return Center(
-      child: Container(
-        width: 200,
-        height: 200,
-        padding: const EdgeInsets.all(20),
-        color: Colors.red,
-        child: const FittedBox(
-            child: Text(
-          "Fitted Box Nedir?",
-          style: TextStyle(fontSize: 100),
-        )),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          crossAxisCount: 2,
+          children: [
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+            randomContainer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container randomContainer() {
+    return Container(
+      width: 200,
+      height: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: context.randomColor.shade300,
+      ),
+      padding: const EdgeInsets.all(2),
+      child: FittedBox(
+        child: Image.network("".randomImage),
       ),
     );
   }
